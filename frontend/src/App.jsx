b226c4ad3779743a8cf0adc5c17b9da4fad71d81
Maaz Ali <maazalisshahid@gmail.com>
2026-08-30 10:34:17 +0500

    chore: scaffold backend and frontend projects with linting and formatting

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="app-container">
+      <h1>TaskFlow</h1>
+      <p>MERN Task & Board Management Application</p>
+    </div>
+  );
+}
+
+export default App;
